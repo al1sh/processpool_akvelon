c24167ad93df96c5388f0be6889cfab5883f9266
Alish Akvelon <firstrestrest@gmail.com>
2018-09-06 17:09:19 +0300

    incapsulation improvements and removal of excess locking

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from multiprocessing import Process, Manager, Lock, cpu_count
+from multiprocessing import Process, Manager, Lock, Value, cpu_count
 
 
 class ProcessPool:
-
     process_list = []
     is_active = False
+    process_limit = cpu_count() * 2
+
+    class ProcessWithCount(Process):
+        def __init__(self, **kwargs):
+            Process.__init__(self, **kwargs)
+            self.task_count = Value('i', 0)
+
 
     @staticmethod
     def worker(current_queue, lock):
@@ -15,11 +21,11 @@ class ProcessPool:
                 print("pid " + str(os.getpid()) + " sleeping, waiting for tasks")
                 sleep(5)
             else:
-                print("pid " + str(os.getpid()) + " received new task")
-                lock.acquire()
-                # get function and arguments from queue
-                func, *args = current_queue.get()
-                lock.release()
+                with lock:
+                    print("pid " + str(os.getpid()) + " received new task")
+                    # get function and arguments from queue
+                    func, *args = current_queue.get()
+
                 # execute the function
                 if args:
                     func(*args)
@@ -29,34 +35,53 @@ class ProcessPool:
 
     @staticmethod
     def process_task(func, *args):
-        # initialize working processes
         if not ProcessPool.is_active:
-            process_limit = cpu_count() * 2
-
-            for i in range(process_limit):
-                current_queue = Manager().Queue()
-                lock = Lock()
-                p = Process(target=ProcessPool.worker, args=(current_queue, lock))
-                ProcessPool.process_list.append({"process_object": p, "queue": current_queue, 'lock': lock})
-
-            for process in ProcessPool.process_list:
-                process['process_object'].start()
-
-            ProcessPool.is_active = True
+            ProcessPool.initialize()
 
         # get the least busy process. acquire locks to check queue for each process
-        for process in ProcessPool.process_list:
-            process['lock'].acquire()
-
-        free_process = min(ProcessPool.process_list, key=lambda x: x['queue'].qsize())
-
-        for process in ProcessPool.process_list:
-            process['lock'].release()
-
-        free_process_lock = free_process['lock']
-        free_process_lock.acquire()
+        free_process = min(ProcessPool.process_list, key=lambda x: x['process_object'].task_count.value)
         free_process['queue'].put((func, *args))
-        free_process_lock.release()
+        free_process['process_object'].task_count.value += 1
+
+        # for process in ProcessPool.process_list:
+        #     process['lock'].acquire()
+        #
+        # free_process = min(ProcessPool.process_list, key=lambda x: x['queue'].qsize())
+        #
+        # for process in ProcessPool.process_list:
+        #     process['lock'].release()
+        #
+        # free_process_lock = free_process['lock']
+        # free_process_lock.acquire()
+        # free_process['queue'].put((func, *args))
+        # free_process_lock.release()
+
+    def add_process(info):
+        ProcessPool.process_list.append(info)
+
+    @staticmethod
+    def initialize():
+        if not ProcessPool.is_active:
+
+            for i in range(ProcessPool.process_limit):
+                current_queue = Manager().Queue()
+                lock = Lock()
+                p = ProcessPool.ProcessWithCount(target=ProcessPool.worker, args=(current_queue, lock))
+                info = {"process_object": p, "queue": current_queue, 'lock': lock}
+                ProcessPool.add_process(info)
+
+            ProcessPool.start_processes()
+            ProcessPool.is_active = True
+
+    @staticmethod
+    def wait_all():
+        for process in ProcessPool.process_list:
+            process['process_object'].join()
+
+    def start_processes():
+        for process in ProcessPool.process_list:
+            process['process_object'].start()
+
 
 
 if __name__ == "__main__":
@@ -70,8 +95,7 @@ if __name__ == "__main__":
     for i in range(run_count):
         ProcessPool.process_task(sleep_task, 5)
 
-    for process in ProcessPool.process_list:
-        process['process_object'].join()
+    ProcessPool.wait_all()
 
     print("main process exited")
 
